68485789721fb98d3cb0ef3973a5e9d2c8ebe862
Lizaosan <lizaosan@gmail.com>
2019-11-18 20:58:50 +0800

    navbar deco

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap'
 /* 以上是 npm 下載套件 */
 import App from './App.vue'
 import router from './router'
