a36d7b71dafc87d2d58604d2adb6457694303847
Lizaosan <lizaosan@gmail.com>
2019-11-20 13:55:17 +0800

    carousel fixed

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,24 +1,75 @@
 <template>
     <div class="container">
-        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <div class="carousel slide" data-ride="carousel" id="carousel-demo">
             <ol class="carousel-indicators">
-                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                <li data-target="#carousel-demo" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel-demo" data-slide-to="1"></li>
+                <li data-target="#carousel-demo" data-slide-to="2"></li>
             </ol>
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                <img src="../assets/carousel_kittyMac.jpg" class="d-block w-100" alt="...">
+                <img class="d-block w-100" src="../assets/carousel_kittyMac.jpg" alt="">
                 </div>
+                <div class="carousel-item">
+                <img class="d-block w-100" src="../assets/carousel_catInRug.jpg" alt="">
+                </div>
+                <div class="carousel-item">
+                <img class="d-block w-100" src="../assets/carousel_closeEye.jpg" alt="">
+                </div>
+                <a href="#carousel-demo" class="carousel-control-prev" data-slide="prev">
+                <span></span>
+                </a>
+                <a href="#carousel-demo" class="carousel-control-next" data-slide="next">
+                <span></span>
+                </a>
             </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
         </div>
     </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+.carousel-control {
+  position: absolute;
+  top: 50%;
+  z-index: 5;
+  display: inline-block;
+}
+.item img {
+  width: 100%;
+  height: auto
+}
+
+ol.carousel-indicators {
+  position: absolute;
+  bottom: 0;
+  margin: 0;
+  left: 0;
+  right: 0;
+  width: auto;
+}
+
+ol.carousel-indicators li,
+ol.carousel-indicators li.active {
+  float: left;
+  width: 33.33%;
+  height: 0.2rem;
+  margin: 0;
+  border-radius: 0;
+  border: 0;
+  background: transparent;
+}
+
+ol.carousel-indicators li.active {
+  background: #D9A97E;
+}
+</style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,56 +1,10 @@
 <template>
-    <!-- <div class="container">
-        <div class="row align-items-center">
-            <div class="col-auto mr-auto">
-                <ul class="nav align-items-center">
-                    <li class="nav-item">
-                        <a class="navbar-brand" @click="paw = ''" href="#">
-                            <img src="../assets/storeIcon.svg" width="120" height="45" class="d-inline-block align-top">
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-primary small" @click="paw = 'food'" href="#">
-                            <i class="fas" :class="{'fa-paw': paw == 'food'}"></i> 貓咪食品
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-primary small" @click="paw = 'supply'" href="#">
-                            <i class="fas" :class="{'fa-paw': paw == 'supply'}"></i> 貓咪居家
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-primary small" @click="paw = 'clean'" href="#">
-                            <i class="fas" :class="{'fa-paw': paw == 'clean'}"></i> 貓咪清潔
-                        </a>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-auto">
-                <ul class="nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                            <i class="fas fa-shopping-cart fa-lg"></i>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div> -->
-    <!-- <div class="container">
-        <div class="row align-items-center">
-            <nav class="navbar">
-                <a class="nav-brand" href="#">
-                    <img src="../assets/storeIcon.svg" style="max-height: 45px;">
-                </a>
-            </nav>
-        </div>
-    </div> -->
-    <nav class="mb-1 navbar navbar-expand-lg navbar-light bg-lignt">
+    <nav class="mb-1 navbar navbar-expand-md navbar-light bg-lignt">
         <div class="container">
             <a class="navbar-brand" @click="paw = ''" href="#">
                 <img src="../assets/storeIcon.svg" width="120" height="45" class="d-inline-block align-top">
             </a>
-            <div class="d-flex flex-row order-md-1 order-lg-2">
+            <div class="d-flex flex-row order-sm-1 order-md-2">
                 <ul class="navbar-nav sub-nav flex-row mr-1">
                     <li class="nav-item">
                         <a href="#" class="nav-link mr-2">
@@ -62,8 +16,7 @@
                     <i class="fas fa-bars fa-lg"></i>
                 </button>
             </div>
-            <!-- 選單 -->
-            <div class="collapse navbar-collapse order-md-2 order-lg-1" id="navbarSupportedContent">
+            <div class="collapse navbar-collapse order-sm-2 order-md-1" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item mr-4">
                         <a class="nav-link text-primary small" @click="paw = 'food'" href="#">
@@ -108,5 +61,4 @@ export default {
 .fas {
   color: #D9A97E;
 }
-
 </style>
