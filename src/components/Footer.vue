39cd84a1ae194deb6eece86fee307360c4782a88
Lizaosan <lizaosan@gmail.com>
2019-12-17 18:12:09 +0800

    fix footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,20 +1,20 @@
 <template>
-    <div class="container-fluid">
     <footer class="pt-4 my-md-5 pt-md-5 border-top">
-    <div class="row align-items-center">
-      <div class="col-6 col-md">
-        <img src="../assets/storeIcon.svg" width="120" height="45" class="d-inline-block align-top">
-        <small class="d-block mb-3 mt-2">© 2015-2020</small>
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-6 col-md">
+            <img src="../assets/storeIcon.svg" width="120" height="45" class="d-inline-block align-top">
+            <small class="d-block mb-3 mt-2">© 2015-2020</small>
+          </div>
+          <div class="col-6 col-md">
+            <h5>About</h5>
+            <ul class="list-unstyled small">
+              <li><a href="https://github.com/lizaosan/wuowo-cat-petshop" target="_blank">此網站專案為個人前端作品</a></li>
+            </ul>
+          </div>
+        </div>
       </div>
-      <div class="col-6 col-md">
-        <h5>About</h5>
-        <ul class="list-unstyled small">
-          <li><a href="https://github.com/lizaosan/wuowo-cat-petshop" target="_blank">此網站專案為個人前端作品</a></li>
-        </ul>
-      </div>
-    </div>
-  </footer>
-    </div>
+    </footer>
 </template>
 
 <style scoped>
